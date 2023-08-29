0d98c80ec88d03d66fea4f7d0e25cb2c6c6f8e7f
Kamil <kaz4486@gmail.com>
2023-08-29 12:24:33 +0200

    Add creating buttons function

--- slider.js
+++ slider.js
@@ -9,6 +9,7 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.image');
+const bottom = document.querySelector('.bottom');
 
 let slideNumber = 1;
 const length = images.length;
@@ -39,3 +40,12 @@ right.addEventListener('click', () => {
 left.addEventListener('click', () => {
   slideNumber > 1 ? prevSlide() : getLastSlide();
 });
+
+for (let i = 0; i < length; i++) {
+  const div = document.createElement('div');
+  div.className = 'button';
+  bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll('.button');
+buttons[0].style.backgroundColor = 'white';
